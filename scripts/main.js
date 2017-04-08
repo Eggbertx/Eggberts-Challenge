b51a6ca62a5eee081404848244c5337c71dc9200
Joshua Merrell <joshuamerrell@gmail.com>
2017-04-08 14:09:06 -0700

    add scripts and tiles

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+//const term = require("term");
+
+const data = require("chipdata");
+const game = require("game");
+system.abort(Color.of("chartreus"));
+data.loadSprites();
+data.loadData();
+game.start();
+system.abort("end!")
